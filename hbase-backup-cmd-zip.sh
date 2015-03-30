55708dd1da635acdac3a627d818135449d9abd2b
Martin Blom <martin.blom@onslip.com>
2015-03-30 16:18:43 +0200

    Added a helper command that zips the database files.

---
+++ hbase-backup-cmd-zip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <HDFS mount point> <HDFS backup path> <HDFS backup folder>"
+    exit 10
+fi
+
+root="$1"
+path="$2"
+name="$3"
+dest=${PWD}/${name}.zip
+
+cd ${root}/${path}
+zip -r "${dest}" "${name}"
